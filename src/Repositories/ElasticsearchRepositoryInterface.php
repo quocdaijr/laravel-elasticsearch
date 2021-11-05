1f18c94703bae4fe3d07cc1e74e77e45f975eadc
quocdaijr <quocdaijr@gmail.com>
2021-11-05 11:43:54 +0700

    first commit package

---
+++ src/Repositories/ElasticsearchRepositoryInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Quocdaijr\Elasticsearch\Repositories;
+
+interface ElasticsearchRepositoryInterface
+{
+    public function search(array $query): array;
+
+    public function find(string|int $id): ?array;
+
+    public function findByAttributes(array $attributes): ?array;
+
+    public function getByAttributes(array $attributes, int $offset = 0, int $limit = 10, array $sortAttributes = []): array;
+
+    public function create(array $data): bool;
+
+    public function updateOrCreate(array $data, array $conditions = []): bool;
+
+    public function update(array $data): bool;
+
+    public function delete(array $data): bool;
+}
